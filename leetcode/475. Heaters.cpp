2ae136fe101ac0b9db6a317403f5bc4119a7722a
AllenCX <allen.caixy@gmail.com>
2016-12-12 22:03:03 +0800

    20161212

---
+++ leetcode/475. Heaters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        int radius = 0;
+        int index = 0;
+        sort(houses.begin(), houses.end());
+        sort(heaters.begin(), heaters.end());
+        for(int i = 0; i < houses.size(); ++i){
+            while(index + 1 < heaters.size() && (abs(heaters[index + 1] - houses[i]) <= abs(heaters[index] - houses[i]))){
+                ++index;
+            }
+            radius = max(radius, abs(heaters[index] - houses[i]));
+        }
+        return radius;
+    }
+};
